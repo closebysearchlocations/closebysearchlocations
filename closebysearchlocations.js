0c4ae3e06d63b973c77777668d7c207395e330f7
closebysearchlocations <107139991+closebysearchlocations@users.noreply.github.com>
2022-06-08 22:41:10 +0530

    Create closebysearchlocations.js

---
+++ closebysearchlocations.js
@@ -0,0 +1,35 @@
+
+      export function configCloseBySearchLocations(consecutiveSearchLocationsFn) {
+        consecutiveSearchLocations = consecutiveSearchLocationsFn;
+      }
+
+      export function setUseOnlyConsecutiveLocations() {
+        useOnlyConsecutiveLocations = true;
+      }
+
+
+      /**
+       * @param location {any}
+       */
+      export function* closeBySearchLocations(location) {
+        let locationToUse = consecutiveSearchLocations(location);
+        if (useOnlyConsecutiveLocations) {
+          yield* locationToUse;
+        }
+        const closeByLocations = [];
+        while (true) {
+          const nextLocation = locationToUse.next().value;
+          if (nextLocation == null) {
+            if (useOnlyConsecutiveLocations) {
+              return;
+            }
+            locationToUse = closeByLocations.shift();
+          } else {
+            yield nextLocation;
+            closeByLocations.push(consecutiveSearchLocations(nextLocation));
+          }
+        }
+      }
+
+      let consecutiveSearchLocations;
+      let useOnlyConsecutiveLocations = false;
